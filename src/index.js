d1f75ea815a3aadaeea3d723de9cea7b021daded
nadezhdanik <nadezhdanik@gmail.com>
2024-07-29 16:30:14 +0300

    feat: solve task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  const pairsString = bracketsConfig.flat().join().replaceAll(",", "");
+  const pairsArray = [];
+  for (let i = 0; i < pairsString.length; i += 2) {
+    pairsArray.push(pairsString[i] + pairsString[i + 1]);
+  }
+
+  let result = str;
+  for (let i = 0; i < str.length / 2; i++) {
+    for (let pair of pairsArray) {
+      if (result.includes(pair)) {
+        result = result.replace(pair, "");
+      }
+    }
+  }
+
+  return !result;
+};
